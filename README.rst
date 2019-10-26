b133b8b958d631f00c54832ee4123f2353bd65ce
Richard Darst <rkd@zgib.net>
2019-10-26 19:39:05 +0300

    initial commit

---
+++ README.rst
@@ -0,0 +1,79 @@
+Clean shell environment for demos
+=================================
+
+You are giving demos on your computer, but you have heavily customized
+``.bashrc``, ``.gitconfig``, etc.  You don't want to have to move
+these out of the way every time.  Linux namespaces to the rescue!
+``clean-shell`` creates a new mount namespace, which can replace the
+values of these config files with empty (or minimal) defaults.  That
+shell, and any subprocesses, will experience this clean environment.
+
+Files cleaned by default: ``.bashrc``, ``~/.ssh/``, and
+``~/.gitconfig``, ``~/.gitignore``.  Furthermore, anything in
+``~/.clean-shell/`` will be replaced (the basename there will have
+``~/`` appended to it) also.  Non-existing original files will be
+ignored (thus, you might make them in the subshell).
+
+Currently only supports Linux, because ``unshare`` only works there.
+But perhaps other operating systems have they own way of doing
+namespaces?  Contributions welcome.
+
+
+
+Installation
+------------
+``clean-shell.py`` is a single-file Python, no installation needed.
+Requires Python>=3.4.
+
+Dependencies: ``unshare`` (``pip install --user unshare``).  This is a
+thin wrapper around the ``unshare`` Linux system call.
+
+
+
+Invocation
+----------
+
+* ``-v`` runs in verbose mode (print all files being altered)
+* ``--persist`` will bind-mount the original template files, so that
+  any modifications are persisted across sessions.  Perhaps useful for
+  recurring demos where you are defining your own aliases and so on.
+  May have currently-unknown interactions with the different
+  directories.
+
+
+
+"Default" files
+---------------
+The first of these values is taken as the replacement value for each
+file:
+
+- ``~/.clean-shell/BASENAME``
+- ``DIRNAME/defaults/BASENAME``, where ``DIRNAME`` is the directory
+  containing ``clean-shell.py``.
+- A empty file, or an empty directory if the path to be replaced ends
+  in ``/``.
+
+
+
+Persistent replacement files
+----------------------------
+
+Sometimes, you don't want to completely reset your ``.bashrc``
+etc. each time you run clean-shell.  You can handle that, too.
+
+You can ::
+   mkdir ~/.csdefaults
+   touch ~/.csdefaults/.bashrc
+   touch ~/.csdefaults/.gitconfig
+
+The files in ``~/.csdefault/`` are bind-mounted in, and if you use
+``--persist`` then any changes inside get persisted to this location.
+
+
+
+Development and maintenance
+---------------------------
+Status: Alpha; improvements, including breaking improvements, are
+welcome.
+
+Editor: Richard Darst, Aalto University.
---
+++ clean-shell.py
@@ -0,0 +1,121 @@
+#!/usr/bin/env python3
+
+import argparse
+import os
+from pathlib import Path
+import shutil
+import subprocess
+import sys
+import tempfile
+import time
+
+import unshare
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--persist', action='store_true',
+                    help="Don't make a temporary copy of the default files, "
+                         "but bind-mount to the original copy.  The "
+                         "originals will be modified in place.")
+parser.add_argument('--verbose', '-v', action='store_true',
+                    help="Be verbose.")
+args = parser.parse_args()
+
+
+FILES = {
+    '~/.bashrc',
+    '~/.gitconfig',
+    '~/.gitignore',
+    '~/.ssh/',
+    }
+# Add any extra files/directories from ~/.clean-shell to the list of
+# stuff to be replaced.
+for p in Path('~/.clean-shell/').expanduser().iterdir():
+    name = p.name
+    if name.endswith('~') or name.endswith('#'):
+        continue
+    name = '~/' + name
+    if p.is_dir():
+        name = name + '/'
+    FILES.add(name)
+
+
+def copy(src, dest):
+    """Copy either file or directory
+
+    Because shutil.copytree annoyingl assumes a directory and fails if
+    not.
+    """
+    if os.path.isdir(src):
+        shutil.copytree(src, dest)
+    else:
+        shutil.copy(src, dest)
+
+
+cs_path = Path(os.path.dirname(__file__))
+default_path = cs_path / 'defaults'
+local_path = Path('~/.clean-shell').expanduser()
+
+UID = os.getuid()
+GID = os.getgid()
+
+tmpdir = tempfile.TemporaryDirectory(prefix='clean-shell-')
+tmp_path = Path(tmpdir.name)
+
+
+#print('phase 1')
+unshare.unshare(unshare.CLONE_NEWNS
+                |unshare.CLONE_NEWUSER)
+open("/proc/self/setgroups", "w").write('deny')
+open("/proc/self/uid_map", 'w').write('0 %s 1'%UID)
+open("/proc/self/gid_map", 'w').write('0 %s 1'%GID)
+
+#print('uid:', os.getuid())
+
+
+# Rebind all files
+for dest in FILES:
+    dest = os.path.expanduser(dest)
+    is_dir = dest.endswith('/')
+    dest = Path(dest)
+    basename = dest.name  # if dir, pathlib.Path removes trailing slash
+
+    # Take values from ~/.clean-shell/
+    if (local_path / basename).exists():
+        src = local_path / basename
+        if not args.persist:
+            copy(str(src), str(tmp_path/basename))
+            src = (tmp_path/basename)
+    # Take values from DIRNAME/defaults/   (DIRNAME=dir of this script)
+    elif (default_path / basename).exists():
+        src = default_path / basename
+        if not args.persist:
+            copy(str(src), str(tmp_path/basename))
+            src = (tmp_path/basename)
+    # Make a new empty file or directory
+    else:
+        src = (tmp_path / basename)
+        if is_dir:
+            src.mkdir()
+        else:
+            src.touch()
+
+    # Mount will fail it if doesn't exist, if we touch it first then it
+    # will persist after we are done.  Better solution to this later?
+    if not dest.exists():
+        print("Ignoring non-existing file %s"%str(dest))
+        continue
+    if args.verbose:
+        print("%s --> %s"%(str(src), str(dest)))
+
+    subprocess.check_call(['mount', '--bind', str(src), str(dest)])
+
+#print('phase 2')
+unshare.unshare(unshare.CLONE_NEWUSER)
+open("/proc/self/setgroups", "w").write('deny')
+open("/proc/self/uid_map", 'w').write('%s 0 1'%UID)
+open("/proc/self/gid_map", 'w').write('%s 0 1'%UID)
+
+#print("executing bash")
+# Spawn instead of exec, so that
+shell = os.environ.get('SHELL', 'bash')
+os.spawnv(os.P_WAIT, shell, [shell])
